0d2f47563ac1be8309edae80d2bf20b882efd263
Gajo Petrovic <gajop@uns.ac.rs>
2013-01-15 17:49:50 +0100

    updated combobox to use 2 space identing, renamed method from ComboBox:Close to ComboBox:_CloseWindow

--- chili/controls/combobox.lua
+++ chili/controls/combobox.lua
@@ -8,8 +8,8 @@ ComboBox = Button:Inherit {
   selected = 1,
   OnSelect = {},
 }
-	
-	
+
+
 local this = ComboBox
 local inherited = this.inherited
 
@@ -19,10 +19,10 @@ function ComboBox:New(obj)
   end
   obj = inherited.New(self,obj)
   if obj.selected == nil then
-      obj.selected = 1
+    obj.selected = 1
   end
   if obj.selected > 0 then
-      obj:Select(obj.selected)
+    obj:Select(obj.selected)
   end
   return obj
 end
@@ -39,106 +39,106 @@ function ComboBox:Select(itemIdx)
   end
 end
 
-function ComboBox:Close()
-    if self._dropDownWindow ~= nil then
-        self._dropDownWindow:Dispose()
-        self._dropDownWindow = nil
-    end
-    if (self.state.pressed) then
-        self.state.pressed = false
-        self:Invalidate()
-        return self
-    end
+function ComboBox:_CloseWindow()
+  if self._dropDownWindow ~= nil then
+    self._dropDownWindow:Dispose()
+    self._dropDownWindow = nil
+  end
+  if (self.state.pressed) then
+    self.state.pressed = false
+    self:Invalidate()
+    return self
+  end
 end
 
 function ComboBox:FocusUpdate()
-    if not self.state.focused then
-        self:Close()
-    end
+  if not self.state.focused then
+    self:_CloseWindow()
+  end
 end
 
 function ComboBox:MouseDown(...)
   self.state.pressed = true
   if self._dropDownWindow == nil then
-      local screen0 = self
-      --local sx, sy = self:LocalToScreen(self.x, self.y)
-      -- TODO: this should work ^^, but it doesn't
-      local sx = self.x
-      local sy = self.y
-      while screen0.parent ~= nil do
-          screen0 = screen0.parent
-          if screen0.classname == "scrollpanel" then
-              sx = sx - screen0.scrollPosX
-              sy = sy - screen0.scrollPosY
-          end
-          sx = sx + screen0.x
-          sy = sy + screen0.y	  
+    local screen0 = self
+    --local sx, sy = self:LocalToScreen(self.x, self.y)
+    -- TODO: this should work ^^, but it doesn't; the following gets the component's current absolute screen position
+    local sx = self.x
+    local sy = self.y
+    while screen0.parent ~= nil do
+      screen0 = screen0.parent
+      if screen0.classname == "scrollpanel" then
+        sx = sx - screen0.scrollPosX
+        sy = sy - screen0.scrollPosY
       end
-      sy = sy + self.height + 10
-      sx = sx + 10
+      sx = sx + screen0.x
+      sy = sy + screen0.y	  
+    end
+    sy = sy + self.height + 10
+    sx = sx + 10
 
-      labels = {}
-      largestStr = 0
-      labelHeight = 20
-      for i = 1, #self.items do
-          largestStr = math.max(largestStr, #self.items[i])
-          local newBtn = Button:New {
-              caption = self.items[i],
-              width = '100%',
-              height = labelHeight,
-              OnMouseUp = { function()
-                  self:Select(i)
-                  self:Close()
-              end }
-          }
-          if i == self.selected then
-              newBtn.backgroundColor = self.focusColor
-          end
-          table.insert(labels, newBtn)
+    labels = {}
+    largestStr = 0
+    labelHeight = 20
+    for i = 1, #self.items do
+      largestStr = math.max(largestStr, #self.items[i])
+      local newBtn = Button:New {
+        caption = self.items[i],
+        width = '100%',
+        height = labelHeight,
+        OnMouseUp = { function()
+          self:Select(i)
+          self:_CloseWindow()
+        end }
+      }
+      if i == self.selected then
+        newBtn.backgroundColor = self.focusColor
       end
-      estimatedWidth = 20 + largestStr * 10
-      estimatedWidth = math.min(estimatedWidth, 500)
-      estimatedWidth = math.max(estimatedWidth, self.width)
+      table.insert(labels, newBtn)
+    end
+    estimatedWidth = 20 + largestStr * 10
+    estimatedWidth = math.min(estimatedWidth, 500)
+    estimatedWidth = math.max(estimatedWidth, self.width)
 
-      local height = math.min(200, labelHeight * #labels) + 10
-      height = math.max(50, height)
-      if sy + height > screen0.height then
-          y = sy - height - 40
-      else
-          y = sy
-      end
-      dropDownWindow = Window:New {
-          parent = screen0,
-          clientWidth = estimatedWidth,
-          clientHeight = height,
-          resizable = false,
-          draggable = false,
-          x = sx,
-          y = y,
+    local height = math.min(200, labelHeight * #labels) + 10
+    height = math.max(50, height)
+    if sy + height > screen0.height then
+      y = sy - height - 40
+    else
+      y = sy
+    end
+    dropDownWindow = Window:New {
+      parent = screen0,
+      clientWidth = estimatedWidth,
+      clientHeight = height,
+      resizable = false,
+      draggable = false,
+      x = sx,
+      y = y,
+      children = {
+        ScrollPanel:New {
+          width = "100%",
+          height = "100%",
+          horizontalScrollBar = false,
           children = {
-              ScrollPanel:New {
-                  width = "100%",
-                  height = "100%",
-                  horizontalScrollBar = false,
-                  children = {
-                      StackPanel:New {
-                          orientation = 'horizontal',
-                          width = '100%',
-                          height = labelHeight * #labels,
-                          resizeItems = false,
-                          padding = {0,0,0,0},
-                          itemPadding = {0,0,0,0},
-                          itemMargin = {0,0,0,0},
-                          children = labels,
-                          borderThickness = 0,
-                      },
-                  },
-              }
-          }
+            StackPanel:New {
+              orientation = 'horizontal',
+              width = '100%',
+              height = labelHeight * #labels,
+              resizeItems = false,
+              padding = {0,0,0,0},
+              itemPadding = {0,0,0,0},
+              itemMargin = {0,0,0,0},
+              children = labels,
+              borderThickness = 0,
+            },
+          },
         }
-      self._dropDownWindow = dropDownWindow
+      }
+    }
+    self._dropDownWindow = dropDownWindow
   else
-      self:Close()
+    self:_CloseWindow()
   end
 
   self:Invalidate()
@@ -146,7 +146,7 @@ function ComboBox:MouseDown(...)
 end
 
 function ComboBox:MouseUp(...)
-    self:Invalidate()
-    return self
-    -- this exists to override Button:MouseUp so it doesn't modify .state.pressed
+  self:Invalidate()
+  return self
+  -- this exists to override Button:MouseUp so it doesn't modify .state.pressed
 end
