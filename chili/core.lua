75d3a1e2ae3dd0f2ea30b2b0c4381dfaa1fe533a
jK <-jk->
2013-02-01 21:11:49 +0100

    fix dull textcolor in Robocracy skin

--- chili/core.lua
+++ chili/core.lua
@@ -2,6 +2,7 @@ local includes = {
   --"headers/autolocalizer.lua",
   "headers/util.lua",
   "headers/links.lua",
+  "headers/backwardcompability.lua",
 
   "handlers/debughandler.lua",
   "handlers/taskhandler.lua",
