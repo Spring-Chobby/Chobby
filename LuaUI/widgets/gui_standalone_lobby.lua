6e7e9ea3fa36885907884b9018fe1c47230fb4a1
Gajo Petrovic <gajop@uns.ac.rs>
2014-09-29 23:57:59 +0200

    added widget that removes extra user interface: Resource bar Minimap Tooltip Console

---
+++ LuaUI/widgets/gui_standalone_lobby.lua
@@ -0,0 +1,15 @@
+function widget:GetInfo()
+  return {
+    name      = "Standalone lobby tools",
+    desc      = "Standalone lobby tools",
+    author    = "gajop",
+    date      = "in the future",
+    license   = "GPL-v2",
+    layer     = 1001,
+    enabled   = true,
+  }
+end
+
+function widget:Initialize()
+    Spring.SendCommands("ResBar 0", "ToolTip 0", "MiniMap 0", "Console 0")
+end
