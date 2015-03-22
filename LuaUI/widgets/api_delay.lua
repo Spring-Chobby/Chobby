384807f002b16500e4f4676aa5aa7115163ad32b
Gajo Petrovic <gajo01@gmail.com>
2015-03-22 21:58:44 +0900

    fix/improve api_delay

--- LuaUI/widgets/api_delay.lua
+++ LuaUI/widgets/api_delay.lua
@@ -14,21 +14,22 @@ end
 
 local currentTime = 0
 local calls = {}
+local ids = 0
 
 -- delay in miliseconds
 local function DelayCall(f, delay)
-	delay = math.floor(delay)
 	local executeTime = currentTime + delay
-	table.insert(calls, {f, executeTime})
+    local id = ids
+	calls[id] = {f, executeTime}
+    ids = ids + 1
 end
 
 function widget:Update()
 	currentTime = os.clock()
-	for i = #calls, 1, -1 do
-		local call = calls[i]
+	for i, call in pairs(calls) do
 		if currentTime >= call[2] then
-			call[1]()
-			calls[i] = nil
+            call[1]()
+            calls[i] = nil
 		end
 	end
 end
