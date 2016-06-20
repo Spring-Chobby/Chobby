1bfbb5406a3688d0f44895fe56c3ab67cbf82c8d
Gajo Petrovic <gajo01@gmail.com>
2016-06-20 18:03:05 +0900

    new lib dir structure

--- luaui/widgets/api_chilifx.lua
+++ luaui/widgets/api_chilifx.lua
@@ -5,7 +5,7 @@ return {
     author    = "gajop",
     date      = "in the future",
     license   = "GPL-v2",
-    layer     = -990,
+    layer     = -999,
     enabled   = true,
     api       = true,
     hidden    = true,
@@ -20,14 +20,15 @@ CHILILFX_DIR = LIBS_DIR .. "chilifx/chilifx/"
 
 function widget:Initialize()
     if not WG.Chili then
-        Spring.Log("ChiliFX", LOG.ERROR, "Missing ChiliUI.")
+        Spring.Log("ChiliFX", LOG.ERROR, "Missing chiliui.")
         widgetHandler:RemoveWidget(widget)
         return
     end
 
     ChiliFX = VFS.Include(CHILILFX_DIR .. "core.lua", nil)
 
-    WG.ChiliFX = ChiliFX
+    local startDisabled = true
+    WG.ChiliFX = ChiliFX(true)
 end
 
 function widget:Shutdown()
