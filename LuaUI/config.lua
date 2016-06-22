16312129f887c26c0ebba992ee959149f57c29a2
Gajo Petrovic <gajo01@gmail.com>
2016-06-22 11:08:25 +0900

    cleanup and update config.lua from upstream

--- LuaUI/config.lua
+++ LuaUI/config.lua
@@ -11,9 +11,6 @@
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
---// randomize each run
-math.randomseed(os.time())
-
 --// 0: disabled
 --// 1: enabled, but can be overriden by widget.GetInfo().unsafe
 --// 2: always enabled
@@ -26,29 +23,32 @@ VFSMODE = VFS.RAW_FIRST
 --// when false, the handler will `compress` some output (e.g. list of started widgets)
 handler.verbose = false or true
 
+local function LoadLibraries()
+	for _, lib in pairs(VFS.SubDirs("libs/")) do
+		local addonDir = lib .. LUA_NAME .. "/widgets"
+		if #VFS.DirList(addonDir) > 0 then
+			table.insert(ADDON_DIRS, addonDir)
+		end
+	end
+end
 
 if LUA_NAME == "LuaUI" then
+	assert(LUAUI_DIRNAME)
+
 	--// Config & Widget Locations
 	ORDER_FILENAME  = LUAUI_DIRNAME .. 'Config/' .. Game.modShortName .. '_order.lua'
 	CONFIG_FILENAME = LUAUI_DIRNAME .. 'Config/' .. Game.modShortName .. '_data.lua'
 	KNOWN_FILENAME  = LUAUI_DIRNAME .. 'Config/' .. Game.modShortName .. '_known.lua'
 
 	ADDON_DIRS     = {
-		LUA_DIRNAME .. 'Addons/';
-		LUA_DIRNAME .. 'Widgets/';
-		LUA_DIRNAME .. 'SystemAddons/';
-		LUA_DIRNAME .. 'SystemWidgets/';
-		LUA_DIRNAME .. 'chili/';
+		LUAUI_DIRNAME .. 'Addons/';
+		LUAUI_DIRNAME .. 'Widgets/';
+		LUAUI_DIRNAME .. 'SystemAddons/';
+		LUAUI_DIRNAME .. 'SystemWidgets/';
+		LUA_DIRNAME .. 'chili/'; -- DEPRECATED: Scheduled for removal in 103
 	}
-	for _, libDir in pairs(VFS.SubDirs("libs/")) do
-		local addonDir = libDir .. "luaui/widgets"
-		if #VFS.DirList(addonDir) > 0 then
-			table.insert(ADDON_DIRS, addonDir)
-		end
-	end
-
 	--// Create the "LuaUI/Config" directory
-	Spring.CreateDir(LUAUI_DIRNAME .. 'Config')
+	Spring.CreateDir(LUAUI_DIRNAME .. 'Config') --FIXME LuaRules!
 
 	handler:Load(LUAUI_DIRNAME .. "SystemWidgets/BlockUserWidgets.lua" --[[, VFS.ZIP]])
 else
@@ -59,10 +59,7 @@ else
 		LUA_DIRNAME .. AddonNames;
 		LUA_DIRNAME .. 'SystemAddons/';
 		LUA_DIRNAME .. 'System' .. AddonNames;
-		LUA_DIRNAME .. 'chili/';
+		LUA_DIRNAME .. 'chili/'; -- DEPRECATED: Scheduled for removal in 103
 	}
 end
-
---// ZK related
---// cache the results of time intensive funcs
-include("Utilities/cache.lua", handler.EG)
+LoadLibraries()
