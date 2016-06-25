5087e79745450f1554594df80cf94ebbf8001a63
TurBoss <j.l.toledano.l@gmail.com>
2016-06-25 08:35:18 +0200

    Auto Join channels

--- libs/liblobby/lobby/wrapper.lua
+++ libs/liblobby/lobby/wrapper.lua
@@ -248,8 +248,6 @@ Wrapper.commands["JOINED"] = Wrapper._OnJoined
 function Wrapper:_OnJoin(chanName)
 
     table.insert(self.myChannels, chanName)
-    Configuration.channels  = self.myChannels
-    Configuration:SaveConfig()
     
     self:super("_OnJoin", chanName)
 end
@@ -262,8 +260,6 @@ function Wrapper:_OnLeft(chanName, userName, reason)
     for i, v in pairs(self.myChannels) do
         if v == chanName then
             table.remove(self.myChannels, i)
-            Configuration.channels  = self.myChannels
-            Configuration:SaveConfig()
             break
         end
     end
