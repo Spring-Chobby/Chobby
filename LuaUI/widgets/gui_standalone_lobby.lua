680624c6761488985d397fb04688eb1fc44b4c6f
TurBoss <j.l.toledano.l@gmail.com>
2016-06-29 07:53:38 +0200

    call LoadCmdColorsConfig to hide selection rectangle

--- LuaUI/widgets/gui_standalone_lobby.lua
+++ LuaUI/widgets/gui_standalone_lobby.lua
@@ -12,18 +12,7 @@ end
 
 function widget:Initialize()
 	Spring.SendCommands("ResBar 0", "ToolTip 0", "Clock 0", "Info 0") --  "Console 0",
-	
-	--Spring.SendCommands("cmdcolors mouseBoxLineWidth 0.0")
-	--Spring.SendCommands("cmdcolors 'mouseBox 0.0 0.0 0.0 0.0'")
-	
-	local f = io.open('cmdcolors.tmp', 'w+')
-	if (f) then
-		f:write('mouseBoxLineWidth 0.0\nmouseBox 0.0 0.0 0.0 0.0')
-		f:close()
-		Spring.SendCommands({'cmdcolors cmdcolors.tmp'})
-	end
-	os.remove('cmdcolors.tmp')
-	
+	Spring.LoadCmdColorsConfig("mouseBox 0.0 0.0 0.0 0.0")
 	gl.SlaveMiniMap(true)
 	gl.ConfigMiniMap(-1,-1,-1,-1)
 end
