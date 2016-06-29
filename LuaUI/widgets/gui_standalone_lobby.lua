3dce35bf248a665ce815344c903316fbca52c39b
TurBoss <j.l.toledano.l@gmail.com>
2016-06-29 20:46:12 +0200

    unify commands

--- LuaUI/widgets/gui_standalone_lobby.lua
+++ LuaUI/widgets/gui_standalone_lobby.lua
@@ -11,8 +11,7 @@ function widget:GetInfo()
 end
 
 function widget:Initialize()
-	Spring.SendCommands("ResBar 0", "ToolTip 0", "Clock 0", "Info 0") --  "Console 0",
-	Spring.SendCommands("set", "MouseDragScrollThreshold" "-1")
+	Spring.SendCommands("ResBar 0", "ToolTip 0", "Clock 0", "Info 0", "set MouseDragScrollThreshold 0") --  "Console 0",
 	Spring.LoadCmdColorsConfig("mouseBox 0.0 0.0 0.0 0.0")
 	gl.SlaveMiniMap(true)
 	gl.ConfigMiniMap(-1,-1,-1,-1)
