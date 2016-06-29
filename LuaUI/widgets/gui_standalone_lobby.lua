ae55a330652fafb1f41666357a0fab81e0232bdf
TurBoss <j.l.toledano.l@gmail.com>
2016-06-29 20:33:52 +0200

    Closes #44

--- LuaUI/widgets/gui_standalone_lobby.lua
+++ LuaUI/widgets/gui_standalone_lobby.lua
@@ -12,6 +12,7 @@ end
 
 function widget:Initialize()
 	Spring.SendCommands("ResBar 0", "ToolTip 0", "Clock 0", "Info 0") --  "Console 0",
+	Spring.SendCommands("set", "MouseDragScrollThreshold" "-1")
 	Spring.LoadCmdColorsConfig("mouseBox 0.0 0.0 0.0 0.0")
 	gl.SlaveMiniMap(true)
 	gl.ConfigMiniMap(-1,-1,-1,-1)
