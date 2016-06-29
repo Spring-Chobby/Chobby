6805b853e84ecce727a5cbd96ac45e8dc666c083
TurBoss <j.l.toledano.l@gmail.com>
2016-06-29 04:21:34 +0200

    temporal solution
    Until i figure out how to use the string method

--- LuaUI/widgets/gui_standalone_lobby.lua
+++ LuaUI/widgets/gui_standalone_lobby.lua
@@ -10,11 +10,20 @@ function widget:GetInfo()
 	}
 end
 
-spSendCommands = Spring.SendCommands
-
 function widget:Initialize()
-	spSendCommands("ResBar 0", "ToolTip 0", "Clock 0", "Info 0") --  "Console 0",
-	spSendCommands('cmdcolors mouseBox 0.0 0.0 0.0 0.0')
+	Spring.SendCommands("ResBar 0", "ToolTip 0", "Clock 0", "Info 0") --  "Console 0",
+	
+	--Spring.SendCommands("cmdcolors mouseBoxLineWidth 0.0")
+	--Spring.SendCommands("cmdcolors 'mouseBox 0.0 0.0 0.0 0.0'")
+	
+	local f = io.open('cmdcolors.tmp', 'w+')
+	if (f) then
+		f:write('mouseBoxLineWidth 0.0\nmouseBox 0.0 0.0 0.0 0.0')
+		f:close()
+		Spring.SendCommands({'cmdcolors cmdcolors.tmp'})
+	end
+	os.remove('cmdcolors.tmp')
+	
 	gl.SlaveMiniMap(true)
 	gl.ConfigMiniMap(-1,-1,-1,-1)
 end
