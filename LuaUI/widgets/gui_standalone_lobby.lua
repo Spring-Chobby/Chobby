e622ee0740195f29c185d943a80cda04c60072e9
TurBoss <j.l.toledano.l@gmail.com>
2016-06-30 21:30:03 +0200

    Use Spring.SetConfigString

--- LuaUI/widgets/gui_standalone_lobby.lua
+++ LuaUI/widgets/gui_standalone_lobby.lua
@@ -11,7 +11,8 @@ function widget:GetInfo()
 end
 
 function widget:Initialize()
-	Spring.SendCommands("ResBar 0", "ToolTip 0", "Clock 0", "Info 0", "set MouseDragScrollThreshold 0") --  "Console 0",
+	Spring.SendCommands("ResBar 0", "ToolTip 0", "Clock 0", "Info 0") --  "Console 0",
+	Spring.SetConfigString("MouseDragScrollThreshold", "0")
 	Spring.LoadCmdColorsConfig("mouseBox 0.0 0.0 0.0 0.0")
 	gl.SlaveMiniMap(true)
 	gl.ConfigMiniMap(-1,-1,-1,-1)
