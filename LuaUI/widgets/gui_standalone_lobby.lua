c68574478ff017d1960fc9340c0d665623ec3b80
TurBoss <j.l.toledano.l@gmail.com>
2016-07-01 05:11:49 +0200

    Remove ground sky and water and set fog black

--- LuaUI/widgets/gui_standalone_lobby.lua
+++ LuaUI/widgets/gui_standalone_lobby.lua
@@ -13,6 +13,10 @@ end
 function widget:Initialize()
 	Spring.SendCommands("ResBar 0", "ToolTip 0", "Clock 0", "Info 0") --  "Console 0",
 	Spring.LoadCmdColorsConfig("mouseBox 0.0 0.0 0.0 0.0")
+	Spring.SetDrawSky(false)
+	Spring.SetDrawWater(false)
+	Spring.SetDrawGround(false)
+	Spring.SetAtmosphere({fogColor = { 0.0, 0.0, 0.0, 0 }})
 	gl.SlaveMiniMap(true)
 	gl.ConfigMiniMap(-1,-1,-1,-1)
 end
