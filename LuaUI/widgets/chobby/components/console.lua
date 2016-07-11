30a3abe9d6ebb5817f1153e9155bb2b9273b9bb4
GoogleFrog <googl3frog@gmail.com>
2016-07-12 00:54:21 +1000

    Added autocompletion to the console. Fixes #94.

--- LuaUI/widgets/chobby/components/console.lua
+++ LuaUI/widgets/chobby/components/console.lua
@@ -27,6 +27,17 @@ function Console:init()
 		right = 100,
 		text = "",
 	}
+	
+	self.ebInputText.KeyPress = function(something, key, ...)
+		if key == Spring.GetKeyCode("tab") then
+			self:Autocomplete(self.ebInputText.text)
+			return false
+		else
+			self.subword = nil
+			return Chili.EditBox.KeyPress(something, key, ...)
+		end
+	end
+	
 	self.btnSubmit = Button:New {
 		bottom = 0,
 		height = 40,
@@ -62,6 +73,54 @@ function Console:init()
 			self.btnSubmit
 		},
 	}
+end
+
+function Console:Autocomplete(textSoFar)
+	if not self.subword then
+		local start = 0
+		for i = 1, 100 do
+			local newStart = (string.find(textSoFar, " ", start) or (start - 1)) + 1
+			
+			if start ~= newStart then
+				start = newStart
+			else
+				break
+			end
+		end
+	
+		self.subword = string.sub(textSoFar, start)
+		self.unmodifiedLength = string.len(textSoFar)
+		local length = string.len(self.subword)
+		
+		self.suggestion = 0
+		self.suggestions = {}
+		
+		if length == 0 then
+			return
+		end
+		
+		local users = lobby:GetUsers()
+		for name, _ in pairs(users) do
+			if name:starts(self.subword) then
+				self.suggestions[#self.suggestions + 1] = string.sub(name, length + 1)
+			end
+		end
+	end
+	
+	if #self.suggestions == 0 then
+		return
+	end
+	
+	self.suggestion = self.suggestion + 1
+	if self.suggestion > #self.suggestions then
+		self.suggestion = 1
+	end
+	
+	self.ebInputText.selStart = self.unmodifiedLength + 1
+	self.ebInputText.selEnd = string.len(textSoFar) + 1
+	self.ebInputText:ClearSelected()
+	
+	self.ebInputText:TextInput(self.suggestions[self.suggestion])
 end
 
 function Console:SendMessage()
