8f5071add1efcd08e7b10bb60bf9d946376d6cdc
GoogleFrog <googl3frog@gmail.com>
2016-07-11 23:47:50 +1000

    Fix interface skirmish crash on leaving a battle.

--- libs/liblobby/lobby/interface_skirmish.lua
+++ libs/liblobby/lobby/interface_skirmish.lua
@@ -153,8 +153,8 @@ function InterfaceSkirmish:SetBattleStatus(status)
 end
 
 function InterfaceSkirmish:LeaveBattle()
-	self:_OnLeftBattle()
-	self:_OnBattleClosed()
+	self:_CallListeners("OnLeftBattle", self.myBattleID, self.myUserName)
+	self:_CallListeners("OnBattleClosed", self.myBattleID)
 	return self
 end
 
