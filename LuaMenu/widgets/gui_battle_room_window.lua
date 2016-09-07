b3a795f5cd8cf6e5748fb551bc1d35c6b8b6e726
GoogleFrog <googl3frog@gmail.com>
2016-09-08 01:21:53 +1000

    Fixed oops with battle window scrollbars. Fixes #283.

--- LuaMenu/widgets/gui_battle_room_window.lua
+++ LuaMenu/widgets/gui_battle_room_window.lua
@@ -22,9 +22,6 @@ local window
 local imHaveMap, imHaveGame
 local lblHaveMap, lblHaveGame
 
--- Function which is called to fix scroll panel sizes
-local ViewResizeUpdate
-
 -- Listeners, needed here so they can be deregistered
 local onBattleClosed
 local onLeftBattle_counter
@@ -823,13 +820,16 @@ local function SetupPlayerPanel(playerParent, spectatorParent, battle, battleID)
 		PositionChildren(mainStackPanel, mainScrollPanel.height)
 	end
 
-	-- Global function
-	function ViewResizeUpdate()
-		if mainStackPanel and spectatorStackPanel then
+	mainScrollPanel.OnResize = {
+		function (obj)
 			PositionChildren(mainStackPanel, mainScrollPanel.height)
+		end
+	}
+	spectatorScrollPanel.OnResize = {
+		function ()
 			PositionChildren(spectatorStackPanel, spectatorScrollPanel.height)
 		end
-	end
+	}
 
 	onUpdateUserTeamStatus = function(listener, userName, allyNumber, isSpectator)
 		if isSpectator then
@@ -1224,8 +1224,6 @@ local function InitializeControls(battleID, oldLobby, topPoportion)
 	end
 	battleLobby:AddListener("OnBattleClosed", onBattleClosed)
 
-	WG.Delay(ViewResizeUpdate, 0.1)
-
 	return window
 end
 
@@ -1392,24 +1390,6 @@ function BattleRoomWindow.LeaveBattle(onlyMultiplayer, onlySingleplayer)
 
 	local tabPanel = WG.Chobby.interfaceRoot.GetBattleStatusWindowHandler()
 	tabPanel.RemoveTab("myBattle", true)
-end
-
-local oldSizeX, oldSizeY
-function widget:ViewResize(vsx, vsy, viewGeometry)
-	oldSizeX, oldSizeY = vsx, vsy
-	if ViewResizeUpdate then
-		WG.Delay(ViewResizeUpdate, 0.1)
-		WG.Delay(ViewResizeUpdate, 0.2)
-		WG.Delay(ViewResizeUpdate, 0.4)
-		WG.Delay(ViewResizeUpdate, 0.8)
-	end
-end
-
-function widget:Update()
-	local screenWidth, screenHeight = Spring.GetWindowGeometry()
-	if screenWidth ~= oldSizeX or screenHeight ~= oldSizeY then
-		widget:ViewResize(screenWidth, screenHeight)
-	end
 end
 
 function widget:Initialize()
