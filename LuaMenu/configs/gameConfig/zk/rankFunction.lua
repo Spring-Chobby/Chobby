a18e9dfec2e9585c9a784202d2f2c46c9df5b286
GoogleFrog <googl3frog@gmail.com>
2016-09-12 23:58:56 +1000

    Better rank progression, 5, 10, 20, 35, 50, 75, 100.

--- LuaMenu/configs/gameConfig/zk/rankFunction.lua
+++ LuaMenu/configs/gameConfig/zk/rankFunction.lua
@@ -5,13 +5,20 @@ local IMAGE_AUTOHOST     = IMAGE_DIR .. "ranks/robot.png"
 local IMAGE_PLAYER       = IMAGE_DIR .. "ranks/player.png"
 local IMAGE_MODERATOR    = IMAGE_DIR .. "ranks/moderator.png"
 
+local rankUnlocks = {5, 10, 20, 35, 50, 75, 100}
+local rankCount = #rankUnlocks
+
 local function GetImageFunction(level, skill, isBot, isModerator)
 	if isBot then
 		return IMAGE_AUTOHOST
 	elseif isModerator then
 		return IMAGE_MODERATOR
 	elseif level and skill then
-		local levelBracket = math.max(0, math.min(7, math.floor(level/10)))
+		local levelBracket = 1
+		while levelBracket <= rankCount and rankUnlocks[levelBracket] <= level do
+			levelBracket = levelBracket + 1
+		end
+		levelBracket = levelBracket - 1
 		
 		local skillBracket = math.max(0, math.min(7, math.floor((skill-1000)/200)))
 		
