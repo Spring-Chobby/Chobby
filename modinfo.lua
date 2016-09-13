1c1c26ddc17685b7579f5052e33cd8a7241b9648
GoogleFrog <googl3frog@gmail.com>
2016-09-14 00:31:47 +1000

    VERSION{v0a.0.9.3} Updated engine version.

--- modinfo.lua
+++ modinfo.lua
@@ -16,9 +16,7 @@ local modinfo = {
 		"Spring content v1",
 	},
 	onlyLocal       = true,
-	-- Note, this is the last working engine version due to:
-	-- https://github.com/spring/spring/commit/7bdceb667c30ccea56362a0af7d835ef1604d813
-	engine          = "103.0.1-98-ge5905db",
+	engine          = "103.0.1-113-g95e5f77",
 }
 
 return modinfo
