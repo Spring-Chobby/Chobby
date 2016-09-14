b509d5a869156855fd455f5b15210857fb773b56
Eshed <ashdnazg@gmail.com>
2016-09-14 16:34:53 +0300

    make widgetprofiler work on new handler
    - I couldn't get drawing to show, so I've replaced it with Echos for now
    - also gathers memory info

--- LuaMenu/widgets/dbg_widgetprofiler.lua
+++ LuaMenu/widgets/dbg_widgetprofiler.lua
@@ -40,7 +40,7 @@ end
 local function Hook(g,name)
 	local spGetTimer = Spring.GetTimer
 	local spDiffTimers = Spring.DiffTimers
-	local widgetName = g.whInfo.name
+	local widgetName = g._info.name
 
 	local realFunc = g[name]
 	g["_old" .. name] = realFunc
@@ -50,15 +50,17 @@ local function Hook(g,name)
 	end
 	local widgetCallinTime = callinTimes[widgetName] or {}
 	callinTimes[widgetName] = widgetCallinTime
-	widgetCallinTime[name] = widgetCallinTime[name] or {0,0}
+	widgetCallinTime[name] = widgetCallinTime[name] or {0,0,0}
 	local timeStats = widgetCallinTime[name]
 
-	local t
+	local t, s
 
 	local helper_func = function(...)
 		local dt = spDiffTimers(spGetTimer(),t)
+		local ds = collectgarbage("count") - s
 		timeStats[1] = timeStats[1] + dt
 		timeStats[2] = timeStats[2] + dt
+		timeStats[3] = timeStats[3] + ds
 		inHook = nil
 		return ...
 	end
@@ -70,6 +72,7 @@ local function Hook(g,name)
 
 		inHook = true
 		t = spGetTimer()
+		s = collectgarbage("count")
 		return helper_func(realFunc(...))
 	end
 
@@ -110,19 +113,15 @@ local function StartHook()
 
 	local wh = widgetHandler
 
-	local CallInsList = {}
-	for name,e in pairs(wh) do
-		local i = name:find("List")
-		if (i)and(type(e)=="table") then
-			CallInsList[#CallInsList+1] = name:sub(1,i-1)
-		end
-	end
-
 	--// hook all existing callins
-	for _,callin in ipairs(CallInsList) do
-		local callinGadgets = wh[callin .. "List"]
-		for _,w in ipairs(callinGadgets or {}) do
-			w[callin] = Hook(w,callin)
+	for callin,_ in pairs(wh.knownCallIns) do
+		for _,ao in handler.addons:iter() do
+			if ao[callin .. "__"] then
+				ao[callin .. "old"] =
+				wh:RemoveAddonCallIn(callin, ao)
+				ao[callin] = Hook(ao,callin)
+				wh:UpdateAddonCallIn(callin, ao)
+			end
 		end
 	end
 
@@ -131,42 +130,29 @@ local function StartHook()
 	--// hook the UpdateCallin function
 
 	oldUpdateWidgetCallIn =	wh.UpdateWidgetCallIn
-	wh.UpdateWidgetCallIn = function(self,name,w)
-		local listName = name .. 'List'
-		local ciList = self[listName]
-		if (ciList) then
-			local func = w[name]
-			if (type(func) == 'function') then
-				if (not IsHook(func)) then
-					w[name] = Hook(w,name)
-				end
-				ArrayInsert(ciList, func, w)
-			else
-				ArrayRemove(ciList, w)
-			end
-			self:UpdateCallIn(name)
-		else
-			print('UpdateWidgetCallIn: bad name: ' .. name)
-		end
+	wh.UpdateWidgetCallIn = function(self,name,ao)
+		wh:RemoveAddonCallIn(name, ao)
+		ao[name] = Hook(ao,name)
+		oldUpdateWidgetCallIn(wh, callin, ao)
 	end
 
-	oldInsertWidget =	wh.InsertWidget
-	widgetHandler.InsertWidget = function(self,widget)
-		if (widget == nil) then
-			return
-		end
+	-- oldInsertWidget =	wh.InsertWidget
+	-- widgetHandler.InsertWidget = function(self,widget)
+		-- if (widget == nil) then
+			-- return
+		-- end
 
-		oldInsertWidget(self,widget)
+		-- oldInsertWidget(self,widget)
 
-		for _,callin in ipairs(CallInsList) do
-			local func = widget[callin]
-			if (type(func) == 'function') then
-				widget[callin] = Hook(widget,callin)
-			end
-		end
-	end
+		-- for _,callin in ipairs(CallInsList) do
+			-- local func = widget[callin]
+			-- if (type(func) == 'function') then
+				-- widget[callin] = Hook(widget,callin)
+			-- end
+		-- end
+	-- end
 
-	Spring.Echo("hooked UpdateCallin: OK")
+	-- Spring.Echo("hooked UpdateCallin: OK")
 end
 
 
@@ -175,20 +161,11 @@ local function StopHook()
 
 	local wh = widgetHandler
 
-	local CallInsList = {}
-	for name,e in pairs(wh) do
-		local i = name:find("List")
-		if (i)and(type(e)=="table") then
-			CallInsList[#CallInsList+1] = name:sub(1,i-1)
-		end
-	end
-
-	--// unhook all existing callins
-	for _,callin in ipairs(CallInsList) do
-		local callinGadgets = wh[callin .. "List"]
-		for _,w in ipairs(callinGadgets or {}) do
-			if (w["_old" .. callin]) then
-				w[callin] = w["_old" .. callin]
+	--// hook all existing callins
+	for callin,_ in pairs(wh.knownCallIns) do
+		for _,ao in handler.addons:iter() do
+			if (ao["_old" .. callin]) then
+				ao[callin] = ao["_old" .. callin]
 			end
 		end
 	end
@@ -197,7 +174,7 @@ local function StopHook()
 
 	--// unhook the UpdateCallin function
 	wh.UpdateWidgetCallIn = oldUpdateWidgetCallIn
-	wh.InsertWidget = oldInsertWidget
+	--wh.InsertWidget = oldInsertWidget
 
 	Spring.Echo("unhooked UpdateCallin: OK")
 end
@@ -208,8 +185,8 @@ end
 local startTimer
 
 function widget:Update()
-	widgetHandler:RemoveWidgetCallIn("Update", self)
 	StartHook()
+	widgetHandler:RemoveWidgetCallIn("Update", self)
 	startTimer = Spring.GetTimer()
 end
 
@@ -219,10 +196,11 @@ end
 
 local tick = 0.1
 local averageTime = 5
-local loadAverages = {}
+local timeLoadAverages = {}
+local spaceLoadAverages = {}
 
 local function CalcLoad(old_load, new_load, t)
-	return old_load*math.exp(-tick/t) + new_load*(1 - math.exp(-tick/t)) 
+	return old_load*math.exp(-tick/t) + new_load*(1 - math.exp(-tick/t))
 	--return (old_load-new_load)*math.exp(-tick/t) + new_load
 end
 
@@ -256,27 +234,35 @@ function widget:DrawScreen()
 		local n = 1
 		for wname,callins in pairs(callinTimes) do
 			local total = 0
-			local cmax	= 0
+			local space = 0
+			local cmax = 0
 			local cmaxname = ""
 			for cname,timeStats in pairs(callins) do
 				total = total + timeStats[1]
+				space = space + timeStats[3]
 				if (timeStats[2]>cmax) then
 					cmax = timeStats[2]
 					cmaxname = cname
 				end
 				timeStats[1] = 0
+				timeStats[3] = 0
 			end
 
-			local load = 100*total/deltaTime
-			loadAverages[wname] = CalcLoad(loadAverages[wname] or load, load, averageTime)
+			local relTime = 100 * total / deltaTime
+			local relSpace = space / deltaTime
+			timeLoadAverages[wname] = CalcLoad(timeLoadAverages[wname] or relTime, relTime, averageTime)
+			spaceLoadAverages[wname] = CalcLoad(spaceLoadAverages[wname] or relSpace, relSpace, averageTime)
 
 			allOverTimeSec = allOverTimeSec + total
 
-			local tLoad = loadAverages[wname]
-			sortedList[n] = {wname..'('..cmaxname..')',tLoad}
+			local tLoad = timeLoadAverages[wname]
+			local sLoad = spaceLoadAverages[wname]
+			if (tLoad > 0.01) or (sLoad > 0.01) then
+				sortedList[n] = {wname..'('..cmaxname..')',tLoad, sLoad}
+				n = n + 1
+			end
 			allOverTime = allOverTime + tLoad
 			if (maximum<tLoad) then maximum=tLoad end
-			n = n + 1
 		end
 
 		table.sort(sortedList,SortFunc)
@@ -289,25 +275,13 @@ function widget:DrawScreen()
 	local vsx, vsy = gl.GetViewSizes()
 	local x,y = vsx-300, vsy-40
 
-	gl.Color(1,1,1,1)
-	gl.BeginText()
 	for i=1,#sortedList do
 		local v = sortedList[i]
 		local wname = v[1]
 		local tLoad = v[2]
-		if maximum > 0 then
-			gl.Rect(x+100-tLoad/maximum*100, y+1-(12)*i, x+100, y+9-(12)*i)
-		end
-		gl.Text(wname, x+150, y+1-(12)*i, 10)
-		gl.Text(('%.3f%%'):format(tLoad), x+105, y+1-(12)*i, 10)
+		local sLoad = v[3]
+		Spring.Echo(wname, tLoad, sLoad)
 	end
-	local i = #sortedList + 1		
-	gl.Text("\255\255\064\064total time", x+150, y-1-(12)*i, 10)
-	gl.Text("\255\255\064\064"..('%.3fs'):format(allOverTimeSec), x+105, y-1-(12)*i, 10)
-	i = i+1
-	gl.Text("\255\255\064\064total FPS cost", x+150, y-1-(12)*i, 10)
-	gl.Text("\255\255\064\064"..('%.1f%%'):format(allOverTime), x+105, y-1-(12)*i, 10)
-	gl.EndText()
 end
 
 --------------------------------------------------------------------------------
