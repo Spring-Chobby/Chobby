2c56ade29f72ddce39910e8565c0fdd9d80911ad
Licho <licho@licho.eu>
2016-10-16 02:27:46 +0200

    FPS limiter for chobby, cap at 50FPS (needs engine merge)

---
+++ LuaMenu/widgets/api_limif_fps.lua
@@ -0,0 +1,35 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+function widget:GetInfo()
+	return {
+		name      = "Limit FPS",
+		desc      = "Limits FPS to save idle CPU cycles",
+		author    = "Licho",
+		date      = "-306210.1318053026",
+		license   = "GPL-v2",
+		layer     = 1001,
+		enabled   = true,
+	}
+end
+
+local MAX_FPS = 50
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- Callins
+
+local lastTimer
+
+function widget:Initialize() 
+	lastTimer = Spring.GetTimer();
+end
+
+function widget:AllowDraw()
+	local timer = Spring.GetTimer();
+	local diff = Spring.DiffTimers(timer, lastTimer)
+	if (diff >= 1/MAX_FPS) then
+		lastTimer = timer
+		return true
+	end 
+	return false
+end
