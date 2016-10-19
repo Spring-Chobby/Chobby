98282caa228116fc2dc7e3666d4958945fdbb677
GoogleFrog <googl3frog@gmail.com>
2016-10-19 19:33:16 +1100

    Key and mouse presses now force a redraw. This makes typing smooth even if you set MAX_FPS to 1.

--- LuaMenu/widgets/api_limit_fps.lua
+++ LuaMenu/widgets/api_limit_fps.lua
@@ -7,7 +7,9 @@ function widget:GetInfo()
 		author    = "Licho",
 		date      = "-306210.1318053026",
 		license   = "GPL-v2",
-		layer     = 1001,
+		layer     = -3000,
+		handler   = true,
+		api       = true, -- Makes KeyPress occur before chili
 		enabled   = true,
 	}
 end
@@ -19,17 +21,32 @@ local MAX_FPS = 15
 -- Callins
 
 local lastTimer
+local forceRedraw = false
 
 function widget:Initialize() 
 	lastTimer = Spring.GetTimer();
 end
 
 function widget:AllowDraw()
+	if forceRedraw then
+		forceRedraw = false
+		return true
+	end
 	local timer = Spring.GetTimer();
 	local diff = Spring.DiffTimers(timer, lastTimer)
 	if (diff >= 1/MAX_FPS) then
 		lastTimer = timer
 		return true
 	end 
+	return false
+end
+
+function widget:MousePress()
+	forceRedraw = true
+	return false
+end
+
+function widget:KeyPress()
+	forceRedraw = true
 	return false
 end
