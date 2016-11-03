629666338c645a15e6370ce18539e4dc4d3e6bbf
GoogleFrog <googl3frog@gmail.com>
2016-11-04 01:42:19 +1100

    Configured evoRTS. Deleted excess images.

---
+++ LuaMenu/configs/gameConfig/evorts/mainConfig.lua
@@ -0,0 +1,54 @@
+local shortname = "evorts"
+
+local mapWhitelist       = VFS.Include(LUA_DIRNAME .. "configs/gameConfig/" .. shortname .. "/mapWhitelist.lua")
+local aiBlacklist        = VFS.Include(LUA_DIRNAME .. "configs/gameConfig/" .. shortname .. "/aiBlacklist.lua")
+local singleplayerConfig = VFS.Include(LUA_DIRNAME .. "configs/gameConfig/" .. shortname .. "/singleplayerMenu.lua")
+local skirmishDefault    = VFS.Include(LUA_DIRNAME .. "configs/gameConfig/" .. shortname .. "/skirmishDefault.lua")
+local defaultModoptions  = VFS.Include(LUA_DIRNAME .. "configs/gameConfig/" .. shortname .. "/ModOptions.lua")
+local rankFunction       = VFS.Include(LUA_DIRNAME .. "configs/gameConfig/zk/rankFunction.lua")
+local backgroundConfig   = VFS.Include(LUA_DIRNAME .. "configs/gameConfig/" .. shortname .. "/skinning/skinConfig.lua")
+
+local settingsConfig, settingsDefault = VFS.Include(LUA_DIRNAME .. "configs/gameConfig/" .. shortname .. "/settingsMenu.lua")
+
+local headingLarge    = LUA_DIRNAME .. "configs/gameConfig/" .. shortname .. "/skinning/headingLarge.png"
+local headingSmall    = LUA_DIRNAME .. "configs/gameConfig/" .. shortname .. "/skinning/headingSmall.png"
+local backgroundImage = LUA_DIRNAME .. "configs/gameConfig/" .. shortname .. "/skinning/background.jpg"
+local taskbarIcon     = LUA_DIRNAME .. "configs/gameConfig/" .. shortname .. "/taskbarLogo.png"
+
+local background = {
+	image           = backgroundImage,
+	backgroundFocus = backgroundConfig.backgroundFocus,
+}
+
+local minimapOverridePath  = LUA_DIRNAME .. "configs/gameConfig/zk/minimapOverride/"
+local minimapThumbnailPath = LUA_DIRNAME .. "configs/gameConfig/zk/minimapThumbnail/"
+
+---------------------------------------------------------------------------------
+-- Getters
+---------------------------------------------------------------------------------
+
+local externalFuncAndData = {
+	dirName                = "evorts",
+	name                   = "Evolution RTS",
+	--defaultGameArchiveName = "??", fill this in.
+	mapWhitelist           = mapWhitelist,
+	aiBlacklist            = aiBlacklist,
+	settingsConfig         = settingsConfig,
+	settingsDefault        = settingsDefault,
+	singleplayerConfig     = singleplayerConfig,
+	skirmishDefault        = skirmishDefault,
+	defaultModoptions      = defaultModoptions,
+	rankFunction           = rankFunction,
+	headingLarge           = headingLarge,
+	headingSmall           = headingSmall,
+	taskbarIcon            = taskbarIcon,
+	background             = background,
+	minimapOverridePath    = minimapOverridePath,
+	minimapThumbnailPath   = minimapThumbnailPath,
+}
+
+function externalFuncAndData.CheckAvailability()
+	return true
+end
+
+return externalFuncAndData
--- LuaMenu/configs/gameConfig/evorts/skirmishDefault.lua
+++ LuaMenu/configs/gameConfig/evorts/skirmishDefault.lua
@@ -1,11 +1,4 @@
-if WG.Chobby.Configuration.singleplayer_mode == 2 then
-	return {
-		map = "Eye_Of_Horus_v2",
-		enemyAI = "shardlua",
-	}
-end
-
 return {
 	map = "Eye_Of_Horus_v2",
-	enemyAI = "NullAI",
+	enemyAI = "shardlua",
 }
