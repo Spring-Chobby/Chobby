f550be73e8d5ffccdd030131da9e539dbbb2027d
L.J. Lim <kingraptorowns@gmail.com>
2016-12-26 15:21:35 +0800

    Rewrite load game widget with SortableList + other fixes

--- LuaMenu/widgets/gui_loadgame.lua
+++ LuaMenu/widgets/gui_loadgame.lua
@@ -21,6 +21,8 @@ local SAVE_DIR = "Saves"
 local SAVE_DIR_LENGTH = string.len(SAVE_DIR) + 2
 local AUTOSAVE_DIR = SAVE_DIR .. "/auto"
 local MAX_SAVES = 999
+
+local Configuration
 --------------------------------------------------------------------------------
 -- Chili elements
 --------------------------------------------------------------------------------
@@ -33,19 +35,12 @@ local ScrollPanel
 local Label
 local Button
 
--- chili elements
-local loadScreen
-local loadGrid	-- TODO not used yet
-local loadScroll
-local loadControls = {}
-
 --------------------------------------------------------------------------------
 -- data
 --------------------------------------------------------------------------------
-local saves = {}
-
 local ingame = false
 
+local saveList = {}
 --------------------------------------------------------------------------------
 -- General utility functions
 --------------------------------------------------------------------------------
@@ -64,7 +59,12 @@ end
 
 local function WriteDate(dateTable)
 	return string.format("%02d/%02d/%04d", dateTable.day, dateTable.month, dateTable.year)
-	.. " " .. string.format("%02d:%02d:%02d", dateTable.hour, dateTable.min, dateTable.sec)
+	.. "\n" .. string.format("%02d:%02d:%02d", dateTable.hour, dateTable.min, dateTable.sec)
+end
+
+local function DateToString(dateTable)
+	return string.format("%04d%02d%02d", dateTable.year, dateTable.month, dateTable.day)
+	.. " " .. string.format("%02d%02d%02d", dateTable.hour, dateTable.min, dateTable.sec)
 end
 
 local function SecondsToClock(seconds)
@@ -73,45 +73,20 @@ local function SecondsToClock(seconds)
 	if seconds <= 0 then
 		return "00:00";
 	else
-		hours = string.format("%02.f", math.floor(seconds/3600));
-		mins = string.format("%02.f", math.floor(seconds/60 - (hours*60)));
-		secs = string.format("%02.f", math.floor(seconds - hours*3600 - mins *60));
-	if seconds >= 3600 then
-		return hours..":"..mins..":"..secs
-	else
-		return mins..":"..secs
-	end
+		hours = string.format("%02d", math.floor(seconds/3600));
+		mins = string.format("%02d", math.floor(seconds/60 - (hours*60)));
+		secs = string.format("%02d", math.floor(seconds - hours*3600 - mins *60));
+		if seconds >= 3600 then
+			return hours..":"..mins..":"..secs
+		else
+			return mins..":"..secs
+		end
 	end
 end
 
 --------------------------------------------------------------------------------
 -- Savegame utlity functions
 --------------------------------------------------------------------------------
--- FIXME: currently unused as it doesn't seem to give the correct order
-local function SortSavesByDate(a, b)
-	if a == nil or b == nil then
-		return false
-	end
-	--Spring.Echo(a.id, b.id, a.date.hour, b.date.hour, a.date.hour>b.date.hour)
-	
-	if a.date.year > b.date.year then return true end
-	if a.date.yday > b.date.yday then return true end
-	if a.date.hour > b.date.hour then return true end
-	if a.date.min > b.date.min then return true end
-	if a.date.sec > b.date.sec then return true end
-	return false
-end
-
-local function SortSavesByFilename(a, b)
-	if a == nil or b == nil then
-		return false
-	end
-	if a.filename and b.filename then
-		return a.filename > b.filename
-	end
-	return false
-end
-
 -- Returns the data stored in a save file
 local function GetSave(path)
 	local ret = nil
@@ -131,7 +106,7 @@ end
 -- Loads the list of save files and their contents
 local function GetSaves()
 	Spring.CreateDir(SAVE_DIR)
-	saves = {}
+	local saves = {}
 	local savefiles = VFS.DirList(SAVE_DIR, "*.lua")
 	for i=1,#savefiles do
 		local path = savefiles[i]
@@ -140,41 +115,15 @@ local function GetSaves()
 			saves[#saves + 1] = saveData
 		end
 	end
-	table.sort(saves, SortSavesByFilename)
+	
+	return saves
 end
 
 local function GetSaveDescText(saveFile)
 	if not saveFile then return "" end
 	return (saveFile.description or "no description")
-		.. "\n" .. saveFile.gameName .. " " .. saveFile.gameVersion
 		.. "\n" .. saveFile.map
 		.. "\n" .. i18n("time_ingame") .. ": " .. SecondsToClock(saveFile.gameframe/30)
-		.. "\n" .. WriteDate(saveFile.date)
-end
-
--- remove the controls representing each savegame
-local function RemoveSaveControls(filename)
-	local function remove(tbl, filename)
-		local parent = nil
-		for i=1,#tbl do
-			local entry = tbl[i]
-			if entry.filename == filename then
-				parent = entry.container.parent
-				entry.container:Dispose()
-				table.remove(tbl, i)
-				break
-			end
-		end
-		parent:Invalidate()
-	end
-	remove(loadControls, filename)
-end
-
-local function RemoveAllSaveControls()
-	for i=1,#loadControls do
-		loadControls[i].container:Dispose()
-	end
-	loadControls = {}
 end
 
 -- redefined later
@@ -214,8 +163,8 @@ local function DeleteSave(filename)
 		local pathNoExtension = SAVE_DIR .. "/" .. filename
 		os.remove(pathNoExtension .. ".lua")
 		os.remove(pathNoExtension .. ".slsf")
-		RemoveSaveControls(filename)
-		-- TODO notify gameside widget to refresh
+		
+		saveList:RemoveItem(filename)
 	end)
 	if (not success) then
 		Spring.Log(widget:GetInfo().name, LOG.ERROR, "Error deleting save " .. filename .. ": " .. err)
@@ -234,169 +183,185 @@ local function SaveLoadConfirmationDialogPopup(filename, saveMode)
 end
 
 -- Makes a button for a save game on the save/load screen
-local function AddSaveEntryButton(saveFile, allowSave)
-	local controlsEntry = {filename = saveFile and saveFile.filename}
-	local parent = allowSave and saveStack or loadStack	--saveScroll or loadScroll	-- FIXME: use grid?
-	
-	controlsEntry.container = Panel:New {
-		parent = parent,
-		height = SAVEGAME_BUTTON_HEIGHT,
-		width = "100%",
+local function AddSaveEntryButton(saveFile, isSaveMode)	
+	local container = Panel:New {
 		x = 0,
-		--y = (SAVEGAME_BUTTON_HEIGHT) * count,
-		caption = "",
-		--backgroundColor = {1,1,1,0},
+		y = 0,
+		width = "100%",
+		resizable = false,
+		draggable = false,
+		padding = {0, 0, 0, 0},
 	}
-	controlsEntry.button = Button:New {
-		parent = controlsEntry.container,
-		x = 4,
-		right = (saveFile and 96 + 8 or 0) + 4,
-		y = 4,
-		bottom = 4,
-		caption = "",
-		OnClick = { function(self)
+	
+	-- load button
+	local actionButton = Button:New {
+		x = 3,
+		y = 3,
+		bottom = 3,
+		width = 80,
+		caption = i18n("load"),
+		classname = "action_button",
+		font = WG.Chobby.Configuration:GetFont(2),
+		OnClick = {
+			function()
 				if ingame then
 					SaveLoadConfirmationDialogPopup(saveFile.filename, false)
 				else
 					LoadGameByFilename(saveFile.filename)
 				end
 			end
+		},
+		parent = container,
+	}
+	
+	-- save name
+	local x = 95
+	local saveName = TextBox:New {
+		name = "saveName",
+		x = x,
+		y = 12,
+		right = 0,
+		height = 20,
+		valign = 'center',
+		fontsize = Configuration:GetFont(2).size,
+		text = saveFile.filename,
+		parent = container,
+	}
+	
+	-- save's modgame name
+	x = x + 200
+	local gameName = TextBox:New {
+		name = "gameName",
+		x = x,
+		y = 12,
+		right = 0,
+		height = 20,
+		valign = 'center',
+		fontsize = Configuration:GetFont(2).size,
+		text = saveFile.gameName .. "\n" .. saveFile.gameVersion,
+		parent = container,
+	}
+	
+	-- save date
+	x = x + 140
+	local saveDate = TextBox:New {
+		name = "saveDate",
+		x = x,
+		y = 12,
+		right = 0,
+		height = 20,
+		valign = 'center',
+		fontsize = Configuration:GetFont(2).size,
+		text = WriteDate(saveFile.date),
+		parent = container,
+	}
+	
+	-- save details
+	x = x + 180
+	local details = TextBox:New {
+		name = "saveDetails",
+		x = x,
+		y = 12,
+		right = 0,
+		height = 20,
+		valign = 'center',
+		fontsize = Configuration:GetFont(2).size,
+		text = GetSaveDescText(saveFile),
+		parent = container,
+	}
+	
+	-- delete button
+	x = x + 200
+	local deleteButton = Button:New {
+		parent = container,
+		x = x,
+		width = 65,
+		y = 4,
+		bottom = 4,
+		caption = i18n("delete"),
+		classname = "action_button",
+		font = WG.Chobby.Configuration:GetFont(2),
+		OnClick = { function(self)
+				WG.Chobby.ConfirmationPopup(function(self) DeleteSave(saveFile.filename) end, i18n("delete_confirm"), nil, 360, 200)
+			end
 		}
 	}
-	--controlsEntry.stack = StackPanel:New {
-	--	parent = controlsEntry.button,
-	--	height = "100%",
-	--	width = "100%",
-	--	orientation = "vertical",
-	--	resizeItems = false,
-	--	autoArrangeV = false,	
-	--}
-	local caption = saveFile and saveFile.filename or i18n("save_new_game")
-	controlsEntry.titleLabel = Label:New {
-		parent = controlsEntry.button,
-		caption = caption,
-		valign = "center",
-		x = 8,
-		y = 8,
-		font = { size = 16 },
-	}
-	if saveFile then
-		controlsEntry.descTextbox = TextBox:New {
-			parent = controlsEntry.button,
-			x = 8,
-			y = 24,
-			right = 8,
-			bottom = 8,
-			text = GetSaveDescText(saveFile),
-			font = { size = 14 },
-		}
-		
-		controlsEntry.deleteButton = Button:New {
-			parent = controlsEntry.container,
-			width = 96,
-			right = 4,
-			y = 4,
-			bottom = 4,
-			caption = i18n("delete"),
-			--backgroundColor = {0.4,0.4,0.4,1},
-			OnClick = { function(self)
-					WG.Chobby.ConfirmationPopup(function(self) DeleteSave(saveFile.filename) end, i18n("delete_confirm"), nil, 360, 200)
-				end
-			}
-		}
-	end
-	return controlsEntry
-end
-
--- Generates the buttons for the savegames on the save/load screen
-local function AddSaveEntryButtons()
-	local startIndex = #saves
-	for i=startIndex,1,-1 do
-		loadControls[#loadControls + 1] = AddSaveEntryButton(saves[i])
-	end
-end
-
-OpenLoadMenu = function(save)
-	GetSaves()
-	RemoveAllSaveControls()
-	AddSaveEntryButtons(save)
-	return loadScreen
+	
+	return saveFile.filename, container, {saveFile.filename, saveFile.gameName .. "" .. saveFile.gameVersion, DateToString(saveFile.date)}
 end
 
 --------------------------------------------------------------------------------
 -- Make Chili controls
 --------------------------------------------------------------------------------
-local function InitializeLoadWindow()
-		loadScroll = ScrollPanel:New {
-		name = 'chobby_loadgame_loadScroll',
-		orientation = "vertical",
-		x = 0,
-		y = 56,
-		width = "100%",
-		bottom = 8,
-		children = {}
-	}
-	loadStack = StackPanel:New {
-		name = 'chobby_loadgame_loadStack',
-		parent = loadScroll,
-		orientation = "vertical",
-		width = "100%",
-		x = 0,
-		y = 0,
-		autosize = true,
-		resizeItems = false,
-		autoArrangeV = false,
-	}
-	loadScreen = Panel:New {
-		--parent = window,
-		name = 'chobby_loadgame_load',
-		x = 0,
-		y = 0,
-		width = "100%",
-		height = "100%",
-		backgroundColor = {0, 0, 0, 0},
-		children = {
-			Label:New {
-				name = 'chobby_loadgame_loadTitle',
-				caption = string.upper(i18n("load")),
-				x = 4,
-				y = 8,
-				font = {
-					size = 40,
-					outlineWidth = 10,
-					outlineHeight = 10,
-					outline = true,
-					outlineColor = Spring.Utilities.CopyTable(OUTLINE_COLOR),
-					autoOutlineColor = false,
-				}
-			},
-			loadScroll,
-		}
-	}
-end
 
-local function InitializeControls()
-	Chili = WG.Chili
-	Window = Chili.Window
-	Panel = Chili.Panel
-	StackPanel = Chili.StackPanel
-	ScrollPanel = Chili.ScrollPanel
-	Label = Chili.Label
-	Button = Chili.Button
+local function InitializeControls(parent)
+	Configuration = WG.Chobby.Configuration
 	
-	-- window to hold things
-	window = Panel:New {
-		name = 'chobby_loadgame_window',
-		x = 0,
-		y = 0,
-		width = "100%",
-		height = "100%",
+	Label:New {
+		x = 15,
+		y = 11,
+		width = 180,
+		height = 30,
+		parent = parentControl,
+		font = Configuration:GetFont(3),
+		caption = string.upper(i18n("load")),
+	}
+	
+	-------------------------
+	-- Generate List
+	-------------------------
+	
+	local listHolder = Control:New {
+		x = 12,
+		right = 15,
+		y = 52,
+		bottom = 15,
+		parent = parent,
 		resizable = false,
 		draggable = false,
 		padding = {0, 0, 0, 0},
-		backgroundColor = {0, 0, 0, 0}
 	}
-	InitializeLoadWindow()
+	
+	local headings = {
+		{name = "Name", x = 97, width = 158},
+		{name = "Game", x = 257, width = 218},
+		{name = "Date", x = 437, width = 138},
+	}
+	
+	saveList = WG.Chobby.SortableList(listHolder, headings, 80, 3)
+
+	local saves = GetSaves()
+	local items = {}
+	for i = 1, #saves do
+		local filename, controls, order = AddSaveEntryButton(saves[i])
+		items[#items + 1] = {filename, controls, order}
+	end
+		
+	saveList:AddItems(items)
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- External Interface
+
+local LoadGameWindow = {}
+
+function LoadGameWindow.GetControl()
+	local window = Control:New {
+		name = "missionHandler",
+		x = "0%",
+		y = "0%",
+		width = "100%",
+		height = "100%",
+		OnParent = {
+			function(obj)
+				if obj:IsEmpty() then
+					InitializeControls(obj)
+				end
+			end
+		},
+	}
+	return window
 end
 
 --------------------------------------------------------------------------------
@@ -412,13 +377,15 @@ function widget:Initialize()
 	CHOBBY_DIR = "LuaMenu/widgets/chobby/"
 	VFS.Include("LuaMenu/widgets/chobby/headers/exports.lua", nil, VFS.RAW_FIRST)
 	
-	InitializeControls()
+	Chili = WG.Chili
+	Window = Chili.Window
+	Panel = Chili.Panel
+	StackPanel = Chili.StackPanel
+	ScrollPanel = Chili.ScrollPanel
+	Label = Chili.Label
+	Button = Chili.Button
 	
-	WG.LoadWindow = {
-		GetWindow = function()
-			return OpenLoadMenu(false)
-		end,
-	}
+	WG.LoadGameWindow = LoadGameWindow
 	
 	local function OnBattleAboutToStart()
 		ingame = true
