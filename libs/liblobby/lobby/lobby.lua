8a793a8a4808a23f686aa733c46a89b2b5ceaf28
GoogleFrog <googl3frog@gmail.com>
2017-02-09 23:59:23 +1100

    Fixed login logic. Should correctly reconnect on server restart. Not sure what to do about local connection loss though since there is no PING.

--- libs/liblobby/lobby/lobby.lua
+++ libs/liblobby/lobby/lobby.lua
@@ -371,7 +371,6 @@ end
 ------------------------
 
 function Lobby:_OnConnect(protocolVersion, springVersion, udpPort, serverMode)
-	self:Login(unpack(self.loginData))
 	if Spring.GetGameName() ~= "" then
 		lobby:SetIngameStatus(true)
 	end
