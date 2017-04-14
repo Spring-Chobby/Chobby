1d8ddbbe93eaeca93d4b42bd41cadc82b38133fe
GoogleFrog <googl3frog@gmail.com>
2017-04-14 16:41:59 +1000

    Lets call circuit stable instead of dev.

--- LuaMenu/configs/gameConfig/zk/defaultSettings/AI/Skirmish/CircuitAIHard32/dev/AIInfo.lua
+++ LuaMenu/configs/gameConfig/zk/defaultSettings/AI/Skirmish/CircuitAIHard32/dev/AIInfo.lua
@@ -21,7 +21,7 @@ local infos = {
 	},
 	{
 		key    = 'version',
-		value  = 'dev', -- AI version - !This comment is used for parsing!
+		value  = 'stable', -- AI version - !This comment is used for parsing!
 	},
 	{
 		key    = 'name',
