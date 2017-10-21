42f6bb3cd436dd95ecea131cef41e6a8432b9a75
GoogleFrog <googl3frog@gmail.com>
2017-10-21 15:57:53 +1100

    Better planet image for planet1.

--- campaign/sample/planets/planet1.lua
+++ campaign/sample/planets/planet1.lua
@@ -5,7 +5,7 @@
 local function GetPlanet(planetUtilities, planetID)
 	
 	--local image = planetUtilities.planetImages[math.floor(math.random()*#planetUtilities.planetImages) + 1]
-	local image = LUA_DIRNAME .. "images/planets/14.png"
+	local image = LUA_DIRNAME .. "images/planets/swamp03.png"
 	
 	local planetData = {
 		name = "Im Jaleth",
