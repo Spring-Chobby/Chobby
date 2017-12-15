8f3d329965f342f35fd8166e8aa1c7e814eda628
GoogleFrog <googl3frog@gmail.com>
2017-12-15 13:58:35 +1100

    Added panels to community window, some of which are unintentionally blank.

--- LuaMenu/widgets/gui_community_window.lua
+++ LuaMenu/widgets/gui_community_window.lua
@@ -17,31 +17,69 @@ end
 --------------------------------------------------------------------------------
 -- Initialization
 
-local function InitializeControls(window)
+local function GetScroll(window, x, right, y, bottom, verticalScrollbar)
+	local holder = Control:New {
+		x = x,
+		y = y,
+		right = right,
+		bottom = bottom,
+		padding = {2, 2, 2, 2},
+		parent = window
+	}
+	return ScrollPanel:New {
+		x = 2,
+		right = 2,
+		y = 2,
+		bottom = 2,
+		horizontalScrollbar = false,
+		verticalScrollbar = verticalScrollbar,
+		padding = {4, 4, 4, 4},
+		--borderColor = {0,0,0,0},
+		--OnResize = {
+		--	function()
+		--	end
+		--},
+		parent = holder
+	}
+end
+
+local function LeaveIntentionallyBlank(scroll)
 	Label:New {
-		x = 15,
-		y = 11,
-		width = 180,
-		height = 30,
-		parent = window,
-		font = WG.Chobby.Configuration:GetFont(3),
-		caption = "Community",
+		x = 0,
+		y = 12,
+		width = 120,
+		height = 20,
+		align = "left",
+		valign = "center",
+		font = WG.Chobby.Configuration:GetFont(0),
+		caption = "(intentionally blank)",
+		parent = scroll
 	}
+end
+
+local function InitializeControls(window)
+	-- Save space
+	--Label:New {
+	--	x = 15,
+	--	y = 11,
+	--	width = 180,
+	--	height = 30,
+	--	parent = window,
+	--	font = WG.Chobby.Configuration:GetFont(3),
+	--	caption = "Community",
+	--}
 	
-	-------------------------
-	-- Download List
-	-------------------------
+	local newsHolder  = GetScroll(window, 0, 0, 0, "62%", true)
+	local leftCenter  = GetScroll(window, 0, "66.6%", "38%", "31%", false)
+	local midCenter   = GetScroll(window, "33.4%", "33.4%", "38%", "31%", false)
+	local rightCenter = GetScroll(window, "66.6%", 0, "38%", "31%", false)
+	local leftLower   = GetScroll(window, 0, "33.4%", "69%", 0, false)
+	local rightLower  = GetScroll(window, "66.6%", 0, "69%", 0, false)
 	
-	local mainHolder = Control:New {
-		x = 12,
-		right = 15,
-		y = 52,
-		bottom = 15,
-		parent = window,
-		resizable = false,
-		draggable = false,
-		padding = {0, 0, 0, 0},
-	}
+	LeaveIntentionallyBlank(midCenter)
+	LeaveIntentionallyBlank(rightCenter)
+	LeaveIntentionallyBlank(leftLower)
+	LeaveIntentionallyBlank(rightLower)
 	
 end
 
