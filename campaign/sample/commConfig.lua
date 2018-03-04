5195ad8258188213e1da51142304e3305ce0debf
GoogleFrog <googl3frog@gmail.com>
2018-03-04 15:32:02 +1100

    CarRepairer nerf desc.

--- campaign/sample/commConfig.lua
+++ campaign/sample/commConfig.lua
@@ -758,7 +758,7 @@ local moduleDefs = {
 	{
 		name = "module_adv_nano",
 		humanName = "CarRepairer's Nanolathe",
-		description = "CarRepairer's Nanolathe - Increases build power by 5._COUNT_",
+		description = "CarRepairer's Nanolathe - Increases build power by 4._COUNT_",
 		image = moduleImagePath .. "module_adv_nano.png",
 		limit = 8,
 		cost = 150,
@@ -766,7 +766,7 @@ local moduleDefs = {
 		slotType = "module",
 		applicationFunction = function (modules, sharedData)
 			-- All comms have 10 BP in their unitDef (even support)
-			sharedData.bonusBuildPower = (sharedData.bonusBuildPower or 0) + 5
+			sharedData.bonusBuildPower = (sharedData.bonusBuildPower or 0) + 4
 		end
 	},
 }
