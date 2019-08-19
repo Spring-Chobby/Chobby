37456e19a2837d42f93fb49a16f2919a6c944430
Gajo Petrovic <gajopetrovic@gmail.com>
2019-08-19 22:43:08 +0900

    disable analytics if wrapper doesn't support it

--- LuaMenu/widgets/api_analytics.lua
+++ LuaMenu/widgets/api_analytics.lua
@@ -40,7 +40,7 @@ function Analytics.SendOnetimeEvent(eventName, value)
 		return
 	end
 	onetimeEvents[eventName] = true
-	if ACTIVE and WG.WrapperLoopback then
+	if ACTIVE and WG.WrapperLoopback ~= nil and WG.WrapperLoopback.GaAddDesignEvent ~= nil then
 		WG.WrapperLoopback.GaAddDesignEvent(eventName, value)
 	else
 		Spring.Echo("DesignEvent", eventName, value)
@@ -55,7 +55,7 @@ function Analytics.SendIndexedRepeatEvent(eventName, value, suffix)
 	if suffix then
 		eventName = eventName .. suffix
 	end
-	if ACTIVE and WG.WrapperLoopback then
+	if ACTIVE and WG.WrapperLoopback ~= nil and WG.WrapperLoopback.GaAddDesignEvent ~= nil then
 		WG.WrapperLoopback.GaAddDesignEvent(eventName, value)
 	else
 		Spring.Echo("DesignEvent", eventName, value)
@@ -64,7 +64,7 @@ end
 
 function Analytics.SendRepeatEvent(eventName, value)
 	eventName = VERSION .. eventName
-	if ACTIVE and WG.WrapperLoopback then
+	if ACTIVE and WG.WrapperLoopback ~= nil and WG.WrapperLoopback.GaAddDesignEvent ~= nil then
 		WG.WrapperLoopback.GaAddDesignEvent(eventName, value)
 	else
 		Spring.Echo("DesignEvent", eventName, value)
@@ -77,7 +77,7 @@ function Analytics.SendErrorEvent(eventName, severity)
 		return
 	end
 	severity = severity or "Info"
-	if ACTIVE and WG.WrapperLoopback then
+	if ACTIVE and WG.WrapperLoopback ~= nil and WG.WrapperLoopback.GaAddErrorEvent ~= nil then
 		WG.WrapperLoopback.GaAddErrorEvent(severity, eventName)
 	else
 		Spring.Echo("ErrorEvent", eventName, severity)
