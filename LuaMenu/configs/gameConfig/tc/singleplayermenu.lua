1cf702a27327ce15e42e06e3965f7fb5de013ffa
Gajo Petrovic <gajopetrovic@gmail.com>
2019-08-21 06:52:02 +0900

    remove tc config https://github.com/Spring-Chobby/Chobby/issues/484; moved to: https://github.com/azaremoth/cursed-chobby

--- LuaMenu/configs/gameConfig/tc/singleplayermenu.lua
+++
@@ -1,7 +0,0 @@
-return {
-	{
-		name = "skirmish",
-		control = WG.BattleRoomWindow.GetSingleplayerControl(VFS.Include(LUA_DIRNAME .. "configs/gameConfig/tc/singleplayerQuickSkirmish.lua")),
-		entryCheck = WG.BattleRoomWindow.SetSingleplayerGame,
-	},
-}
