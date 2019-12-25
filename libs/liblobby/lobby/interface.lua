e08276bfedc29b06c0e1161bf870f6d7200bbd72
silentwings <3885930+silentwings@users.noreply.github.com>
2019-12-25 21:08:44 +0000

    fix case in TASSERVER

--- libs/liblobby/lobby/interface.lua
+++ libs/liblobby/lobby/interface.lua
@@ -303,8 +303,8 @@ end
 function Interface:_OnTASServer(protocolVersion, springVersion, udpPort, serverMode)
 	self:_OnConnect(protocolVersion, springVersion, udpPort, serverMode)
 end
-Interface.commands["TASServer"] = Interface._OnTASServer
-Interface.commandPattern["TASServer"] = "(%S+)%s+(%S+)%s+(%S+)%s+(%S+)"
+Interface.commands["TASSERVER"] = Interface._OnTASServer
+Interface.commandPattern["TASSERVER"] = "(%S+)%s+(%S+)%s+(%S+)%s+(%S+)"
 
 function Interface:_OnMOTD(message)
 	-- IGNORED
