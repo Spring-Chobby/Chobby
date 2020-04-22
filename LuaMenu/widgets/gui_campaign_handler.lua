e66e9f14f9b31fa63a5b039cb29ce08ba32e179c
Porkchop <longlivetheporkchop@gmail.com>
2020-04-22 14:23:23 -0600

    Include x,y change from #739

--- LuaMenu/widgets/gui_campaign_handler.lua
+++ LuaMenu/widgets/gui_campaign_handler.lua
@@ -732,8 +732,8 @@ local function SelectPlanet(popupOverlay, planetHandler, planetID, planetData, s
 	}
 
 	local planetDesc = TextBox:New {
-		x = 8,
-		y = "30%",
+		x = 20,
+		y = "25%",
 		right = 4,
 		bottom = "25%",
 		text = planetData.infoDisplay.text,
