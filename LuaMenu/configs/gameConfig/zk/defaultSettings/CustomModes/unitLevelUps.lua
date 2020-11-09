920394669c5a4605210b4bbc635c7f3ca460cfe8
GoogleFrog <googl3frog@gmail.com>
2020-11-09 19:44:17 +1100

    Comment the custom mode file since, if this takes off, it is likely to copypasta.

--- LuaMenu/configs/gameConfig/zk/defaultSettings/CustomModes/unitLevelUps.lua
+++ LuaMenu/configs/gameConfig/zk/defaultSettings/CustomModes/unitLevelUps.lua
@@ -1,8 +1,14 @@
 return {
+	-- Name is required, try not to clash with other names.
 	name = "Unit Level Ups",
+	-- roomType can be Custom, 1v1, Team, FFA, Chicken. It should probably be Custom.
 	roomType = "Custom",
+	-- Map should match map name exactly, as is written below the minimap in the battleroom.
 	map = nil, -- "Fairyland 1.31",
+	-- Game should match game name exactly, as is shown left of the map when the game is selected.
 	game = "Unit Level Ups v5",
+	-- options is a table of Adv Options. The keys are internal name.
+	-- All the keys can be found here: https://github.com/ZeroK-RTS/Zero-K/blob/master/ModOptions.lua
 	options = {
 		--metalmult = 2,
 	},
