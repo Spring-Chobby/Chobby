8c09cfcc1482fe2e53d75a26f40830e3073bae91
Gajo Petrovic <gajopetrovic@gmail.com>
2020-12-31 16:12:33 +0900

    make it possible to disable ZK's map filtering system, close https://github.com/Spring-Chobby/Chobby/issues/524

--- LuaMenu/widgets/gui_settings_window.lua
+++ LuaMenu/widgets/gui_settings_window.lua
@@ -920,7 +920,9 @@ local function GetLobbyTabControls()
 	children[#children + 1], offset = AddCheckboxSetting(offset, i18n("ingame_notifcations"), "ingameNotifcations", true)
 	children[#children + 1], offset = AddCheckboxSetting(offset, i18n("non_friend_notifications"), "nonFriendNotifications", true)
 	children[#children + 1], offset = AddCheckboxSetting(offset, i18n("notifyForAllChat"), "notifyForAllChat", false)
-	children[#children + 1], offset = AddCheckboxSetting(offset, i18n("only_featured_maps"), "onlyShowFeaturedMaps", true)
+	if not Configuration.gameConfig.disableZKMapFiltering then
+		children[#children + 1], offset = AddCheckboxSetting(offset, i18n("only_featured_maps"), "onlyShowFeaturedMaps", true)
+	end
 	children[#children + 1], offset = AddCheckboxSetting(offset, i18n("simplifiedSkirmishSetup"), "simplifiedSkirmishSetup", true)
 	children[#children + 1], offset = AddCheckboxSetting(offset, i18n("simple_ai_list"), "simpleAiList", true)
 	children[#children + 1], offset = AddCheckboxSetting(offset, i18n("animate_lobby"), "animate_lobby", true)
