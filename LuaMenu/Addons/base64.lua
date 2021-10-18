4ac267571d8d1ea0c19ec471661b7f1c0e8e7a6d
Beherith <mysterme@gmail.com>
2021-10-18 17:11:54 +0200

    Spring.Utilities.Base64EncodeMem(data) does not reallocate result strings for every concat, uses table.concat instead.

--- LuaMenu/Addons/base64.lua
+++ LuaMenu/Addons/base64.lua
@@ -49,6 +49,19 @@ function Spring.Utilities.Base64Encode(data)
 	return result
 end
 
+-- Encode but faster and more memory efficient
+function Spring.Utilities.Base64EncodeMem(data)
+	local bytes = {}
+	local result = {}
+	for spos=0,string.len(data)-1,3 do
+		for byte=1,3 do bytes[byte] = string.byte(string.sub(data,(spos+byte))) or 0 end
+		result[#result+1] = string.format('%s%s%s%s',base64chars[rsh(bytes[1],2)],base64chars[lor(lsh((bytes[1] % 4),4), rsh(bytes[2],4))] or "=",((#data-spos) > 1) and base64chars[lor(lsh(bytes[2] % 16,2), rsh(bytes[3],6))] or "=",((#data-spos) > 2) and base64chars[(bytes[3] % 64)] or "=")
+	end
+	result = table.concat(result)
+	--Spring.Echo(result)
+	return result
+end
+
 -- decryption table
 local base64bytes = {['A']=0,['B']=1,['C']=2,['D']=3,['E']=4,['F']=5,['G']=6,['H']=7,['I']=8,['J']=9,['K']=10,['L']=11,['M']=12,['N']=13,['O']=14,['P']=15,['Q']=16,['R']=17,['S']=18,['T']=19,['U']=20,['V']=21,['W']=22,['X']=23,['Y']=24,['Z']=25,['a']=26,['b']=27,['c']=28,['d']=29,['e']=30,['f']=31,['g']=32,['h']=33,['i']=34,['j']=35,['k']=36,['l']=37,['m']=38,['n']=39,['o']=40,['p']=41,['q']=42,['r']=43,['s']=44,['t']=45,['u']=46,['v']=47,['w']=48,['x']=49,['y']=50,['z']=51,['0']=52,['1']=53,['2']=54,['3']=55,['4']=56,['5']=57,['6']=58,['7']=59,['8']=60,['9']=61,['-']=62,['_']=63,['=']=nil}
 
