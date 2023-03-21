2d0b993d402fe9028b6bc8a7daa2e6d224d15460
Gajo Petrovic <gajopetrovic@gmail.com>
2023-03-21 19:28:07 +0900

    one less global

--- LuaMenu/widgets/chobby/components/list_window.lua
+++ LuaMenu/widgets/chobby/components/list_window.lua
@@ -229,7 +229,7 @@ function ListWindow:UpdateFilters()
 end
 
 function ListWindow:SwapPlaces(panel1, panel2)
-	tmp = panel1.index
+	local tmp = panel1.index
 
 	local x1,y1,w1,h1 = panel1._relativeBounds.left, panel1.y, panel1._relativeBounds.width, panel1.height
 	local x2,y2,w2,h2 = panel2._relativeBounds.left, panel2.y, panel2._relativeBounds.width, panel2.height
